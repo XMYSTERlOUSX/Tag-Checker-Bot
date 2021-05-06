b583972f5a3ad1544a8eb1872f06d59f023ccd8f
꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-05-06 14:07:31 +0530

    Update tag-check.py

--- plugins/tag-check.py
+++ plugins/tag-check.py
@@ -20,6 +20,7 @@ async def anon_filter(_, __, m: Message):
 
 anonymous = filters.create(anon_filter)
 
+@Client.on_message(filters.group)
 async def flt_admin(_, client, message):
     user = await client.get_chat_member(message.chat.id, message.from_user.id)
     return True if user.status in ["administrator", "creator"] else False
