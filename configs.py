51d0483258777879d46f505882c087fda613af93
꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-05-06 12:36:13 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,5 +9,8 @@ class Config(object):
     API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
     API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
     Tag_Check_Group = os.getenv("Tag_Check_Group", None) #the group id where you want to chech user's tags
+    Allowed_USERS = list(map(int, os.getenv("Allowed_USERS").split())) # Allowed user's ids to be without the tag except admins.(seperate ids with space)
+    Tag_Name = os.getenv("Tag_Name", None) #the Tag name you want to set
+    Bot_url = os.getenv("Bot_url", None) # The bot's link
 
     
