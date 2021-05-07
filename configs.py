eff7e6ee9910b35573e6727b17fe890d8cff848b
꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-05-07 09:55:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,5 +11,6 @@ class Config(object):
     Tag_Check_Group = os.getenv("Tag_Check_Group", None) # Your Group id where you want to check user's tags(with -100 in front)
     Tag_Name = os.getenv("Tag_Name", None) # The Tag name you want to set for checking
     Bot_url = os.getenv("Bot_url", None) # The bot's telegram link. Ex:-https://t.me/your_bot?start=start Note:- Include "?start=start" at the end of the url!
+    group1_name = os.getenv("group1_name", None)
 
     
