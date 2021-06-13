753b540e45692e14d4a622394bc0bd95ced43c7d
꧁✪≋✘_.мүsтεяισυs_.✘≋✪꧂ <73080587+XMYSTERlOUSX@users.noreply.github.com>
2021-06-13 14:24:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,6 +11,5 @@ class Config(object):
     Tag_Check_Group = os.getenv("Tag_Check_Group", None) # Your Group id where you want to check user's tags.(must include -100 in front)
     Tag_Name = os.getenv("Tag_Name", None) # The Tag name you want to set for checking
     Log_Group_id = os.getenv("Log_Group_id", None) #Make a log group to save the details of who has been unmuted. Input the group id here.(must include -100 in front)
-    Bot_url = os.getenv("Bot_url", None) # The bot's telegram link. Ex:-https://t.me/your_bot?start=start  Note:- Include "?start=start" at the end of the url!
 
     
